49e1014859b85f8afce456c57fb9409ea7fb1e01
SoundSkote <98184441+SoundSkote@users.noreply.github.com>
2024-06-09 21:13:20 +0100

    Update BlogPost.js

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -21,7 +21,7 @@ function BlogPost() {
   
     return (
       <div className="flex flex-col pt-40 justify-center items-center">
-        <ReactMarkdown className="markdown text-left space-y-5 w-1/2">{fm(post.toString()).body}</ReactMarkdown>
+        <ReactMarkdown className="markdown text-left item-center space-y-5 w-1/2">{fm(post.toString()).body}</ReactMarkdown>
       </div>
     );
   }
