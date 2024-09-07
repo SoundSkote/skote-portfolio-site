7dc0681243963e09a016608db58120d85f22ee44
SoundSkote <98184441+SoundSkote@users.noreply.github.com>
2024-09-07 11:52:54 +0100

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -39,7 +39,7 @@ function About() {
               <li>Still Wakes the Deep</li>
               <li>Vampire: The Masquerade - Bloodlines 2</li>
               <li>Silent Hill 2 remake</li>
-              <li>Banishers: Ghosts of New Eden</li>
+              <li>Retrieval</li>
             </ul>
             <ul className='lg:w-1/3'>
               <li className='font-extrabold underline pb-5 list-none'>My hobbies:</li>
