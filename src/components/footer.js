97ea0fac813a1cdeaa9a7c59123e54adfebd89bf
SoundSkote <98184441+SoundSkote@users.noreply.github.com>
2025-03-26 15:42:38 +0000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -21,9 +21,10 @@ function Footer() {
             <img className="h-[7rem] w-auto pt-5 transition-all duration-300 group-hover:scale-125" src={email} alt='email'/>
           </a>
           <a className="flex h-[7rem] w-auto pt-6 justify-center" target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/georgios-georgakis/'><img className="h-5/6 w-auto transition-all duration-300 hover:scale-125" src={linkedIn} alt='linked in'/></a>
+          <a className="flex h-[7rem] w-auto pt-6 justify-center" target="_blank" rel="noreferrer" href='https://bsky.app/profile/georgiosaudio.bsky.social/'><img className="h-5/6 w-auto transition-all duration-300 hover:scale-125" src={bluesky} alt='bluesky'/></a>
         </div>
       </div>
-      <h1 className='text-xs md:text-base md:order-2'>Copyright© 2023-24 Created by <a target="_blank" rel="noreferrer" href='https://www.kwtsos.bio' className='hover:text-slate-500'>Kostas</a></h1>
+      <h1 className='text-xs md:text-base md:order-2'>Copyright© 2025-26 Created by <a target="_blank" rel="noreferrer" href='https://www.kwtsos.bio' className='hover:text-slate-500'>Kostas</a></h1>
     </div>
   );
 }
