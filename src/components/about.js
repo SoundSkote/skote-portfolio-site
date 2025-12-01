39599c6404bbf75f84c3950fe980d71488e648d9
SoundSkote <98184441+SoundSkote@users.noreply.github.com>
2025-12-01 08:50:49 +0000

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -36,8 +36,8 @@ function About() {
             <ul className='lg:w-1/3'>
               <li className='font-extrabold underline pb-5 list-none'>Can't wait to play:</li>
               <li>SPINE</li>
-              <li>ARC Raiders</li>
-              <li>Vampire: The Masquerade - Bloodlines 2</li>
+              <li>007 First Light</li>
+              <li>Resident Evil Requiem</li>
               <li>Cronos: The New Dawn</li>
               <li>Deadlock</li>
             </ul>
